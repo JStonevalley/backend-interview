b95a9b4c9540562085588f35e77da315b5a46100
Jonas Stendahl <jonas@sellpy.se>
2022-11-04 09:51:37 +0100

    Basic models for selling items

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,27 @@ const itemSchema = new Schema({
   images: [String]
 })
 
+const itemSaleSchema = new Schema({
+  item: String,
+  value: {
+    amount: Number,
+    currency: String
+  },
+  endedAt: Date
+})
+
+const itemOfferSchema = new Schema({
+  itemSale: String,
+  price: {
+    amount: Number,
+    currency: String
+  },
+  endedAt: Date
+})
+
 
 module.exports = {
-  Item: model('Item', itemSchema)
+  Item: model('Item', itemSchema),
+  ItemSale: model('ItemSale', itemSaleSchema),
+  ItemOffer: model('ItemOffer', itemOfferSchema)
 }
